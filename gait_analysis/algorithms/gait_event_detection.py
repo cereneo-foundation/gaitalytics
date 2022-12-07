498d29ccc32d8816220e03dea1ad9539009eba27
anboen <andre.boeni@cereneo.foundation>
2022-12-07 13:27:10 +0100

    documentation

--- gait_analysis/algorithms/gait_event_detection.py
+++ gait_analysis/algorithms/gait_event_detection.py
@@ -3,7 +3,10 @@
 # As input you will probably need 3 nparray's (numpy) of the same length
 # hip points, heel point and frame number
 # As output i want lists of the events with the frame number in it
-def zenis_gait_event_detection(heel_point_array=[], hip_point_array=[], frame_no=[]):
+import pandas as pd
+
+
+def zenis_gait_event_detection(heel_point_array: pd.DataFrame, hip_point_array: pd.DataFrame) -> pd.DataFrame:
     HS = [1, 23, 56]
     TO = [12, 45, 65]
     return [HS, TO]
