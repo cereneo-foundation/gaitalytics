902514ce418af40317ff29a49a8bf078a43517ba
SimonZamora <simon.zamorabustamante@epfl.ch>
2023-01-23 16:43:06 +0100

    Debug event and test_events

--- test/gait_analysis/test_events.py
+++ test/gait_analysis/test_events.py
@@ -10,12 +10,12 @@ class TestZenisEvents(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.path = "test/data/test.c3d"
+        cls.path = r"../data/test.c3d"
 
     def test_good_case(self):
         mapper = HBMToCGM2Mapper()
         acq = btkTools.smartReader(self.path, mapper.get_translator())
-        btkTools.smartWriter(acq, "test/data/markers.c3d")
+        btkTools.smartWriter(acq, r"../data/markers.c3d")
         mapper.calculate_missing_markers(acq)
         GaitEventDetectorFactory().get_zenis_detector().detect_events(acq)
         self.assertEqual(acq.GetEvents().GetItemNumber(), 622, "Zenis event detector does not work")
@@ -26,13 +26,13 @@ class TestForcePlateEvents(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.path = r"../test/data/test.c3d"
+        cls.path = r"../data/test.c3d"
 
     def test_good_case(self):
         acq = btkTools.smartReader(self.path)
         GaitEventDetectorFactory().get_force_plate_detector().detect_events(acq)
-        btkTools.smartWriter(acq, r"../test/data/out.c3d")
-        self.assertEqual(acq.GetEvents().GetItemNumber(), 622, "Zenis event detector does not work")
+        btkTools.smartWriter(acq, r"../data/out.c3d")
+        self.assertEqual(acq.GetEvents().GetItemNumber(), 2, "ForcePlate event detector does not work")
 
 
 if __name__ == '__main__':
