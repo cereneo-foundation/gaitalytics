3625bd6da42b88a3e0541f40dbc3a3d4a74faf3d
aboen <andre.boeni@cereneo.foundation>
2023-01-23 13:49:15 +0100

    finish model mapper add zenis algorithm remove events fomr test.c3d

--- test/gait_analysis/test_models.py
+++ test/gait_analysis/test_models.py
@@ -1,6 +1,6 @@
 import unittest
 from pyCGM2.Tools import btkTools
-from btk import btkAcquisitionFileReader
+from pyCGM2.Events import eventProcedures, eventFilters
 from gait_analysis.models import HBMToCGM2Mapper
 
 
@@ -10,7 +10,6 @@ class TestHBMToCGM2Mapper(unittest.TestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.path = "test/data/test.c3d"
-        cls.acq = btkTools.smartReader(cls.path)
         cls._NAME_MAPPINGS = ("RASI",
                               "LASI",
                               "RPSI",
@@ -23,6 +22,8 @@ class TestHBMToCGM2Mapper(unittest.TestCase):
                               "LMED",
                               "LFMH",  # TODO: not same
                               "LVMH",
+                              "LHEE",
+                              "LTOE",
                               "RTHI",
                               "RKNE",
                               "RKNM",
@@ -31,17 +32,21 @@ class TestHBMToCGM2Mapper(unittest.TestCase):
                               "RMED",
                               "RFMH",  # TODO: not same
                               "RVMH",
+                              "RHEE",
+                              "RTOE",
                               "CLAV",
-                              "T2")
+                              "T2") # TODO: not same
 
     def test_good_case(self):
+        acq = btkTools.smartReader(self.path)
         mapper = HBMToCGM2Mapper()
-        acq = mapper.map_to_cgm2(self.acq)
+        mapper.map_to_cgm2(acq)
         for marker_name in self._NAME_MAPPINGS:
             try:
                 acq.GetPoint(marker_name)
             except RuntimeError:
                 self.assertTrue(False, f"{marker_name} not found")
+
 
 
 if __name__ == '__main__':
