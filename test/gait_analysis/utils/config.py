52189549834df23209fda8d7925678636e773c4f
andre.boeni <andre.boeni@cereneo.foundation>
2023-06-16 08:44:22 +0200

    bug fix

---
+++ test/gait_analysis/utils/config.py
@@ -0,0 +1,20 @@
+import unittest
+
+from gait_analysis.utils.config import ConfigProvider
+
+SETTINGS_FILE = "settings/hbm_pig.yaml"
+
+class ConfigTests(unittest.TestCase):
+    def test_read_config(self):
+        configs = ConfigProvider()
+        configs.read_configs(SETTINGS_FILE)
+        self.assertEqual(configs.MODEL_MAPPING.left_heel.value, "LHEE")
+
+    def test_get_translated_label(self):
+        configs = ConfigProvider()
+        configs.read_configs(SETTINGS_FILE)
+        self.assertEqual(configs.MODEL_MAPPING.left_heel.value, "LHEE")
+
+
+if __name__ == '__main__':
+    unittest.main()
