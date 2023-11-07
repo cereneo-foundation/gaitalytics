b96e5bd68bea685dbabc8202b6c42d0de758fff4
André Böni <andre.boeni@cereneo.foundation>
2023-11-07 08:35:21 +0100

    + restructure out folders in examples

--- examples/pipeline_hbm_extraction_normalisation.py
+++ examples/pipeline_hbm_extraction_normalisation.py
@@ -7,15 +7,19 @@ from gaitalytics import utils
 def main():
     settings_file = "settings/hbm_pig.yaml"
     file_path = "./test/data/Baseline.5.c3d"
-    buffered_path = "./out"
+    buffered_path_raw = "./raw"
+    buffered_path_norm = "./norm"
 
     configs = utils.ConfigProvider(settings_file)
-    if not os.path.exists(buffered_path):
-        os.mkdir(buffered_path)
-        cycle_data = api.extract_cycles(file_path, configs, buffer_output_path=buffered_path)
+    if not os.path.exists(buffered_path_raw):
+        os.mkdir(buffered_path_raw)
+        cycle_data = api.extract_cycles(file_path, configs, buffer_output_path=buffered_path_raw)
     else:
-        cycle_data = api.extract_cycles_buffered(buffered_path, configs).get_raw_cycle_points()
-    api.normalise_cycles(file_path, cycle_data, buffer_output_path=buffered_path)
+        cycle_data = api.extract_cycles_buffered(buffered_path_raw, configs).get_raw_cycle_points()
+
+    if not os.path.exists(buffered_path_norm):
+        os.mkdir(buffered_path_norm)
+    api.normalise_cycles(file_path, cycle_data, buffer_output_path=buffered_path_norm)
 
 
 # __name__
